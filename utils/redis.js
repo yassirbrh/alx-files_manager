8bddd072136bd351d77abf2d24980812114f73bb
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-03-10 23:45:30 +0000

    Creating the file utils/redis.js that contains the class RedisClient.

---
+++ utils/redis.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.isConnected = true;
+    this.client.on('error', (err) => {
+      this.isConnected = false;
+      console.error('Redis client not connected to the server:', err.toString());
+    });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async get(key) {
+    return promisify(this.client.get).bind(this.client)(key);
+  }
+
+  async set(key, value, duration) {
+    return promisify(this.client.setex).bind(this.client)(key, duration, value);
+  }
+
+  async del(key) {
+    return promisify(this.client.del).bind(this.client)(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
