e681ca924adeab00de3dbecbed87a4336a2ef3d5
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-03-11 18:20:00 +0000

    class DBClient that creates a client to MongoDB.

--- utils/db.js
+++ utils/db.js
@@ -3,26 +3,26 @@
 import { MongoClient } from 'mongodb';
 
 class DBClient {
-	constructor() {
-		this.host = process.env.DB_HOST || 'localhost';
-		this.port = process.env.DB_PORT || 27017;
-		this.database = process.env.DB_DATABASE || 'files_manager';
-		const uri = `mongodb://${this.host}:${this.port}/${this.database}`;
-		this.client = new MongoClient(uri);
-		this.client.connect();
-	}
+  constructor() {
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+    const uri = `mongodb://${this.host}:${this.port}/${this.database}`;
+    this.client = new MongoClient(uri);
+    this.client.connect();
+  }
 
-	isAlive() {
-		return this.client.isConnected();
-	}
+  isAlive() {
+    return this.client.isConnected();
+  }
 
-	async nbUsers() {
-		return this.client.db().collection('users').countDocuments();
-	}
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
 
-	async nbFiles() {
-		return this.client.db().collection('files').countDocuments();
-	}
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
 }
 
 const dbClient = new DBClient();
