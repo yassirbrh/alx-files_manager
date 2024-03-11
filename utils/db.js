5fa4c0286537e683c2985cc59381b244382940c4
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-03-11 18:19:12 +0000

    class DBClient that creates a client to MongoDB.

---
+++ utils/db.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+	constructor() {
+		this.host = process.env.DB_HOST || 'localhost';
+		this.port = process.env.DB_PORT || 27017;
+		this.database = process.env.DB_DATABASE || 'files_manager';
+		const uri = `mongodb://${this.host}:${this.port}/${this.database}`;
+		this.client = new MongoClient(uri);
+		this.client.connect();
+	}
+
+	isAlive() {
+		return this.client.isConnected();
+	}
+
+	async nbUsers() {
+		return this.client.db().collection('users').countDocuments();
+	}
+
+	async nbFiles() {
+		return this.client.db().collection('files').countDocuments();
+	}
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
